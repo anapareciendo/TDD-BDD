8dd590fbcb0918e0db5ce4345c6d23d3fbb96a0b
Ana <ana.apariciora@gmail.com>
2020-10-23 17:46:45 +0200

    Add Concordion project

---
+++ helloworld-concordion/src/main/java/com/curso/GreetingService.java
@@ -0,0 +1,10 @@
+package com.curso;
+
+public class GreetingService {
+
+	public String greetings(final String username) {
+		final StringBuilder sb = new StringBuilder();
+		return sb.append("Hola ").append(username).append("!").toString();
+	}
+
+}
